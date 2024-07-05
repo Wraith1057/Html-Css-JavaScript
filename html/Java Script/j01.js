cde8c99b0c082828e1df6b88d3d782cadadbe37c
Wraith1057 <143244705+Wraith1057@users.noreply.github.com>
2024-07-05 17:09:14 +0530

    Add files via upload

---
+++ html/Java Script/j01.js
@@ -0,0 +1,3 @@
+console.log("Hey Buddy")
+console.log("Lest's Begin our JS journey")
+console.log("I am Wraith")
---
+++ html/Java Script/j02.js
@@ -0,0 +1,5 @@
+fullname = "Abheesh";
+age = 18;
+price = 99.99;
+x = null;
+y = undefined;
